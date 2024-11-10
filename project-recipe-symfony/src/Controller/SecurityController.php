7704f9dff0a3a4d2cbe42db960288621737b0e01
ZAKI Fatiha <133255331+FATI-A@users.noreply.github.com>
2024-11-10 11:48:24 +0100

    🔐 add a register

--- project-recipe-symfony/src/Controller/SecurityController.php
+++ project-recipe-symfony/src/Controller/SecurityController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,7 +14,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route('/connexion', name: 'security.login', methods: ['GET', 'POST'])]
-    public function login(AuthenticationUtils $authenticationUtils ): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         return $this->render('pages/security/login.html.twig', [
             'last_username' => $authenticationUtils->getLastUsername(),
@@ -28,5 +32,39 @@ class SecurityController extends AbstractController
     public function logout()
     {
         // nothing to doo here it will be automatically
+    }
+
+
+/**
+ * this controller allow us to register
+ *
+ * @param Request $request
+ * @param EntityManagerInterface $manager
+ * @return Response
+ */
+    #[Route('/inscription', name: 'security.registration', methods: ['GET', 'POST'])]
+    public function registration(
+        Request $request,
+        EntityManagerInterface $manager
+    ): Response {
+
+        $user = new User();
+        $user->setRoles(['ROLE_USER']);
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $this->addFlash(
+                'success',
+                'votre compte a bien éte créer '
+            );
+            $manager->persist($user);
+            $manager->flush();
+            return $this->redirectToRoute('security.login');
+        }
+
+        return $this->render('pages/security/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
